b73acfbc9e5214ac75ad5a5436adb2e8d9973d1f
Lecci123 <edwardjanuar911@gmail.com>
2025-04-29 02:47:20 +0700

    Update catub_config.py

--- userbot/Config/catub_config.py
+++ userbot/Config/catub_config.py
@@ -1,19 +1,22 @@
-""" Config values will be loaded from here"""
+"""Config values will be loaded from here"""
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~# CatUserBot #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # Copyright (C) 2020-2023 by TgCatUB@Github.
-
 # This file is part of: https://github.com/TgCatUB/catuserbot
 # and is released under the "GNU v3.0 License Agreement".
-
 # Please see: https://github.com/TgCatUB/catuserbot/blob/master/LICENSE
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 import os
 
-ENV = bool(os.environ.get("ENV", False))
 
-if ENV:
-    from sample_config import Config  # noqa
-elif os.path.exists("config.py"):
-    from config import Development as Config  # noqa
+class Config:
+    API_ID = os.environ.get("API_ID")
+    API_HASH = os.environ.get("API_HASH")
+    BOTLOG_CHATID = os.environ.get("BOTLOG_CHATID")
+    STRING_SESSION = os.environ.get("STRING_SESSION")
+
+    # Tambahan opsional lain jika dibutuhkan
+    # OWNER_ID = os.environ.get("OWNER_ID")
+    # DATABASE_URL = os.environ.get("DATABASE_URL")
+    # dan lain-lain...
